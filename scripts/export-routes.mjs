4b05e98473422b51d53ff09e453bf823eae29444
wwgame28 <neimaambachew@gmail.com>
2026-09-24 20:29:16 +0900

    Export tattoo constructor route

--- scripts/export-routes.mjs
+++ scripts/export-routes.mjs
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-const routes=['works','flash','prices','about','process','faq','booking',...['floral','cathedral','snake','angel','rose','moon','tribal','stars'].map(s=>'works/'+s)];
+const routes=['works','flash','constructor','prices','about','process','faq','booking',...['floral','cathedral','snake','angel','rose','moon','tribal','stars'].map(s=>'works/'+s)];
 const html=fs.readFileSync('dist/index.html','utf8');
 for(const route of routes){fs.mkdirSync(path.join('dist',route),{recursive:true});fs.writeFileSync(path.join('dist',route,'index.html'),html)}
 fs.writeFileSync('dist/404.html',html);fs.cpSync('assets','dist/assets',{recursive:true});
